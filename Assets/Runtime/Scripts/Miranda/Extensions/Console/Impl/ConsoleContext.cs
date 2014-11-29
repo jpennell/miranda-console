5d95d02a4c55ba57c8d3d602b3e8d91cf7728856
James Pennell <jpennell@ea.com>
2014-11-29 11:21:08 -0500

    Initial commit

---
+++ Assets/Runtime/Scripts/Miranda/Extensions/Console/Impl/ConsoleContext.cs
@@ -0,0 +1,26 @@
+﻿using Jamespennell.Injection.Extensions.Signals.Impl;
+
+namespace Jamespennell.Injection.Extensions.Console.Impl
+{
+	public abstract class ConsoleContext : SignalContext
+	{
+		#region Constructors
+
+		public ConsoleContext() : base()
+		{
+
+		}
+
+		#endregion
+
+		#region Methods
+
+		protected override void MapPluginBindings()
+		{
+			base.MapPluginBindings();
+		}
+
+		#endregion
+
+	}
+}
